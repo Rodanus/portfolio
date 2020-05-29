4fdb5dcb4b4bd98f86a0c31b86c6d793ae445063
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2020-05-29 17:27:21 +0300

    style: Apply prettier formatting style

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 (function () {
-    const body = document.getElementsByTagName("BODY"),
-        navMenu = document.querySelector(".nav-list"),
-        arrowDown = document.querySelector(".arrow-down-con");
+  const body = document.getElementsByTagName("BODY"),
+    navMenu = document.querySelector(".nav-list"),
+    arrowDown = document.querySelector(".arrow-down-con");
 
-    let visible = false;
+  let visible = false;
 
-    document.addEventListener("click", e => {
-        if (e.target.classList.contains("menu-button-img") || e.target.classList.contains("close-button-img")) {
-            navMenu.classList.toggle("show");
-            arrowDown.style.visibility = visible ? "visible" : "hidden";
-            visible = !visible;
-        }
-    });
-})()
+  document.addEventListener("click", e => {
+    if (
+      e.target.classList.contains("menu-button-img") ||
+      e.target.classList.contains("close-button-img")
+    ) {
+      navMenu.classList.toggle("show");
+      arrowDown.style.visibility = visible ? "visible" : "hidden";
+      visible = !visible;
+    }
+  });
+})();
