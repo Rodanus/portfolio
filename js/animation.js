3e18281444d9b0594362fb7daf578aa268f3587f
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2020-07-18 21:04:44 +0300

    refactor: Change some code for improvement

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,10 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const tl = gsap.timeline({ defaults: { duration: 1, opacity: 0 } });
+const tl = gsap.timeline({ defaults: { duration: 1, autoAlpha: 0 } });
 
 tl.to(".loading", {
-  width: "80%",
-  height: "80%",
+  y: "-100%",
+  delay: 3,
   onComplete: () => {
     // Make sure that div.loading is hidden so that nav links are clickable again.
     const loading = document.querySelector(".loading");
@@ -20,46 +20,36 @@ tl.to(".loading", {
   })
 
   .from(".skills-logos-design", {
-    xPercent: "+100"
+    duration: 2
   })
-
-  .from(".header", {
-    y: "-100",
-    delay: 0.2
-  })
+  .from(".header", {})
   .from(".arrow-down-con", {
     y: "-100%",
     ease: "power.in"
-    // onComplete: () => {
-    //   document.querySelector("body").style.overflow = "visible";
-    // }
   });
 
 gsap.from(".about-info-con", {
   scrollTrigger: {
     trigger: ".about",
     start: "top center"
-    // markers: true
   },
   duration: 1.5,
-  opacity: 0
+  autoAlpha: 0
 });
 
 gsap.from(".skill-logo", {
   scrollTrigger: {
     trigger: ".skills",
-    // markers: true,
     start: "top center"
   },
   duration: 1,
-  opacity: 0,
+  autoAlpha: 0,
   stagger: 0.4
 });
 
 const endOfPageTl = gsap.timeline({
   scrollTrigger: {
     trigger: ".projects",
-    // markers: { startColor: "blue", endColor: "yellow" },
     start: "top center"
   }
 });
@@ -68,15 +58,15 @@ endOfPageTl
   .from(".project", {
     duration: 1,
     scale: 0.9,
-    opacity: 0,
+    autoAlpha: 0,
     stagger: 0.8
   })
   .from(".contact-email", {
     duration: 1,
-    opacity: 0
+    autoAlpha: 0
   })
   .from(".footer-link", {
     duration: 1,
-    opacity: 0,
+    autoAlpha: 0,
     stagger: 0.6
   });
