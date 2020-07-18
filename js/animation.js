8356529fccd1df00e518db43c9793c6d0d710e74
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2020-07-18 21:06:42 +0300

    Disable scrolling when loading animation plays

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,57 @@
+// Disable scrolling when playing the animation
+// https://stackoverflow.com/a/4770179/12993793
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+let keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+let supportsPassive = false;
+try {
+  window.addEventListener(
+    "test",
+    null,
+    Object.defineProperty({}, "passive", {
+      get: function () {
+        supportsPassive = true;
+      }
+    })
+  );
+} catch (e) {}
+
+let wheelOpt = supportsPassive ? { passive: false } : false,
+  wheelEvent =
+    "onwheel" in document.createElement("div") ? "wheel" : "mousewheel";
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener("DOMMouseScroll", preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener("touchmove", preventDefault, wheelOpt); // mobile
+  window.addEventListener("keydown", preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener("DOMMouseScroll", preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+  window.removeEventListener("touchmove", preventDefault, wheelOpt);
+  window.removeEventListener("keydown", preventDefaultForScrollKeys, false);
+}
+
+disableScroll();
+
 gsap.registerPlugin(ScrollTrigger);
 
 const tl = gsap.timeline({ defaults: { duration: 1, autoAlpha: 0 } });
@@ -10,18 +64,16 @@ tl.to(".loading", {
     const loading = document.querySelector(".loading");
 
     loading.style.visibility = "hidden";
-  },
-  y: "-100%",
-  delay: 3
+
+    enableScroll();
+  }
 })
 
   .from(".greetings-heading", {
     xPercent: "-100"
   })
 
-  .from(".skills-logos-design", {
-    duration: 2
-  })
+  .from(".skills-logos-design", {})
   .from(".header", {})
   .from(".arrow-down-con", {
     y: "-100%",
